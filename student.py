7c9e529d4304d71bd38952e27e5edab9e0872b19
bdel09 <bmcdelbarrio@gmail.com>
2025-09-24 22:04:37 -0400

    Create student.py

---
+++ student.py
@@ -0,0 +1,13 @@
+#Name: Benjamin Del Barrio
+#Email: Benjamin.delbarrio31@myhunter.cuny.edu
+import turtle
+...
+...
+...
+t = turtle.Turtle()
+def five_integers():
+    for i in range(5):
+        integer = int(input("Enter an Integer:"))
+...     t.left(integer)
+...     t.forward(100)
+five_integers()
